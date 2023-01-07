e461aefe3eb76f0dfed89f97873a02920d729412
arafath-am <arafathamer.m@gmail.com>
2023-01-07 09:14:03 -0600

    create REST endpoints

---
+++ backend/src/main/java/com/campuscart/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.campuscart.repository;
+
+import com.campuscart.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContainingIgnoreCaseOrCategoryContainingIgnoreCase(String name, String category);
+}
