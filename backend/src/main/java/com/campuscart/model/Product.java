ed13b6416d5c5abe393accea6d807978e1910b44
arafath-am <arafathamer.m@gmail.com>
2023-01-07 09:18:57 -0600

    create REST endpoints

---
+++ backend/src/main/java/com/campuscart/model/Product.java
@@ -0,0 +1,90 @@
+package com.campuscart.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String category;
+
+    @Column(length = 1000)
+    private String description;
+
+    @NotNull
+    private BigDecimal price;
+
+    @Min(0)
+    private int stockQuantity;
+
+    public Product() {
+    }
+
+    public Product(String name, String category, String description, BigDecimal price, int stockQuantity) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
